a6aeff52d372dc861b42e78541cf27b79c8f23a5
Joe Querin <joecue@gmail.com>
2016-08-18 12:06:13 -0400

    Initial Commit

---
+++ includes/class-lccc-shared-content-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.lorainccc.edu
+ * @since      1.0.0
+ *
+ * @package    Lccc_Shared_Content
+ * @subpackage Lccc_Shared_Content/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Lccc_Shared_Content
+ * @subpackage Lccc_Shared_Content/includes
+ * @author     LCCC Web Dev Team <notice@lorainccc.edu>
+ */
+class Lccc_Shared_Content_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-lccc-shared-content-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://www.lorainccc.edu
+ * @since      1.0.0
+ *
+ * @package    Lccc_Shared_Content
+ * @subpackage Lccc_Shared_Content/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Lccc_Shared_Content
+ * @subpackage Lccc_Shared_Content/includes
+ * @author     LCCC Web Dev Team <notice@lorainccc.edu>
+ */
+class Lccc_Shared_Content_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
